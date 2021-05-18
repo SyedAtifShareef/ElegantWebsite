dcb2b2755c2bac864b7a1b4161a5e572568aa51a
SyedAtifShareef <160418735103@mjcollege.ac.in>
2021-05-18 19:34:18 +0530

    Update form-handler.php
    removed @

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email= $_POST['email'];
 $subject= $_POST['subject'];
 $message= $_POST['message'];
 
-$email_from="info@university-of-engineering.netlify.app";
+$email_from="university-of-engineering.netlify.app";
 
 
 $email_subject="Query from Website";
