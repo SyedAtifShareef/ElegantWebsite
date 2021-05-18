2269b337b10d1b727028a0e97f937cdff5465dea
SYED SHAREEF <160418735103@mjcollege.ac.in>
2021-05-18 18:57:16 +0530

    complete webiste with php form handler

---
+++ form-handler.php
@@ -0,0 +1,35 @@
+<?php
+$name= $_post['name'];
+$visitor_email= $_POST['email'];
+$subject= $_POST['subject'];
+$message= $_POST['message'];
+
+$email_from="http://127.0.0.1:5500/contact.html";
+
+
+$email_subject="Query from Website";
+
+
+$email_body="User Name: $name.\n".
+            "User Name: $visitor_email.\n".
+            "Subject: $subject.\n".
+            "User Message: $message.\n";
+
+
+
+$to='ateef.shareef000@gmail.com';
+
+$headers="From: $email_from \r\n";
+
+$headers .= "Reply-To $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+?>
+
+
+
+
+
+
